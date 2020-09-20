a7f49accd575d9a0474a0a35f6e818d7133ffa2d
saranf <qorwlsdk1995@naver.com>
2020-09-21 01:15:52 +0900

    const example

---
+++ const_let.js
@@ -0,0 +1,16 @@
+if (true){
+    var x = 3;
+}
+
+console.log("x : "+x);
+
+if(true){
+    const y=3;
+}
+
+console.log("y : "+y);
+
+/*
+var 은 함수 스코프를 가지므로 if문 블록과 관계 없이 접근 가능 
+const 와 let은 블록 스코프를 가지므로 블록 밖애서는 변수에 접근 불가능 
+*/
