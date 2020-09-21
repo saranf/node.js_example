1ff6e5c1d2dfbf32e3ca7e9b9181d57f3c4ba2ce
saranf <qorwlsdk1995@naver.com>
2020-09-21 19:39:49 +0900

    화살표 함수

--- object_literal.js
+++ object_literal.js
@@ -1,3 +1,4 @@
+//예전문법
 var sayNode = function() {
     console.log('Node');
 };
@@ -17,6 +18,7 @@ console.log(oldObject.ES6);
 
 /////////////////////////////////////////////////////////
 
+//ES2015문법
 var es2 = 'ES';
 
 const newObject = {
@@ -31,6 +33,8 @@ newObject.sayJs2();
 console.log(newObject.ES6);
 
 /*
-    ES5 
+    예전 문법에서는 ES6 라는 속성명을 만들려면 객체 리터럴(oldObject) 바깥에서 [es + 6] 를 해야 했습니다.
+    ES2015 문법에서는 객체 리터럴 안에 동적 속성을 선언해도 됩니다.
+    newObject  안에서 [es + 6 ] 가 속성명으로 바로 사용되고 있습니다.
 */
 
